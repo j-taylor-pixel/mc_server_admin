fc03de20ef7b4140c54a252809c90585ef47147d
j-taylor-pixel <j42taylo@uwaterloo.ca>
2022-07-11 16:52:12 -0400

    Added gitignore file, moved a lot of code into helpers
    Added upload world funcitonality
    added download world functiopnality

--- create_mc_server.ps1
+++ create_mc_server.ps1
@@ -1,22 +1,33 @@
-# flag to do additional steps if testing
-
+# stage var to do additional steps if testing
 #$stage = "Testing"
 $stage = "DontTearDownTesting"
 
-# if docker not running, then start it
-# this could be factored into another ps script for maintanability
-
-
-
-
 #so we always start in an empty environment
-#this doesnt really remove the container
 if ("Testing" -eq $stage)
 {
     #tear down container while testing 
     docker rm /mc
 }
 
+# i guess this should fail if mc is already created
 #accept eula
 docker run -e EULA=TRUE -d -p 25565:25565 --name mc itzg/minecraft-server
+
+#upload desired world if present
+#ask for confirmation
+Write-Output "Paste in world folder into upload_world"
+$uploadTrue= Read-Host -Prompt "Type y to upload world file"
+
+if("y" -eq $uploadTrue)
+{
+    #this might not need to be factored out if it isnt used anywhere else
+    $PSScriptRoot
+
+    & "$PSScriptRoot\helper_scripts\upload_world.ps1"
+    
+    # clear upload world folder
+    #nah just do manually   
+    #$uploadPath = PSScriptRoot\upload_world
+    #Remove-Item -path $uploadPath -recurse
+}
 
---
+++ helper_scripts/upload_world.ps1
@@ -0,0 +1,6 @@
+Write-Output "Pretending to upload world..."
+#uploads the world file in the upload_world folder
+#docker cp  ./upload_world/World mc:/data
+
+#currently untested right now
+
